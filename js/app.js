fdba8a46d81874a0ee65a58cf097d4a733535e75
lirbja <lirbja@gmail.com>
2014-03-20 16:15:00 -0400

    hadouken sound

--- js/app.js
+++ js/app.js
@@ -10,17 +10,29 @@ $(document).ready(function() {
 
 	/* new stuff */
 	.mousedown(function() {
-		console.log('mousedown');
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show()
 		//animate it to the right of the screen
+		.animate(
+		  {'left': '400px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '-129px');
+		  }
+		);
 	})
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
+	});
+});
 
-})
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
 
